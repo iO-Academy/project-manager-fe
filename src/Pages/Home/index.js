58033f0251914a6481ef00c4bbdfe0c6b782bc5a
Mike Oram <mike.oram@mayden.co.uk>
2022-08-01 15:38:29 +0100

    create client filter

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,30 +1,53 @@
 import Project from "../../Molecules/Project";
 import {useContext, useEffect, useState} from "react";
-import {projectsURL} from "../../config";
+import {clientsURL, projectsURL} from "../../config";
 import handleError from "../../utils/ErrorHandler";
 import LocaleContext from "../../utils/LocaleContext";
 
 export default function Home() {
     const [projects, setProjects] = useState([])
-    const [error, setError] = useState(false)
+    const [clients, setClients] = useState([])
+    const [projectError, setProjectsError] = useState(false)
+    const [clientError, setClientError] = useState(false)
+    const [clientFilter, setClientFilter] = useState('all')
 
     const locale = useContext(LocaleContext)
 
     const getProjects = async () => {
         try {
-            const response = await fetch(projectsURL + '?locale=' + locale)
-            if (await handleError(response, setError)) {
+            let requestURL = projectsURL + '?locale=' + locale
+            if (clientFilter !== 'all') {
+                requestURL = projectsURL + '?client=' + clientFilter + '&locale=' + locale
+            }
+            const response = await fetch(requestURL)
+            if (await handleError(response, setProjectsError)) {
                 const data = await response.json()
                 setProjects(data.data)
             }
         } catch(e) {
-            setError('Unable to retrieve data')
+            setProjectsError('Unable to retrieve data')
+        }
+    }
+
+    const getClients = async () => {
+        try {
+            const response = await fetch(clientsURL)
+            if (await handleError(response, setClientError)) {
+                const data = await response.json()
+                setClients(data.data)
+            }
+        } catch(e) {
+            setClientError('Unable to retrieve client data')
         }
     }
 
     useEffect(() => {
         getProjects()
-    }, [locale])
+    }, [locale, clientFilter])
+
+    useEffect(() => {
+        getClients()
+    }, [])
 
     return (
         <>
@@ -32,15 +55,32 @@ export default function Home() {
                 <div className='col-12'>
                     <h1>Projects</h1>
                 </div>
+                <div className="col-12">
+                    <h5>Filter projects by client:</h5>
+                    <select value={clientFilter} onChange={(e) => {setClientFilter(e.target.value)}}>
+                        {
+                            !clientError &&
+                            <option value="all">All clients</option>
+                        }
+                        {
+                            clientError &&
+                            <option selected disabled>{clientError}</option>
+                        }
+                        {
+                            clients &&
+                            clients.map(client => <option key={client.id} value={client.id}>{client.name}</option>)
+                        }
+                    </select>
+                </div>
             </div>
             <div className='row'>
                 {
-                    error &&
-                    <div className="col-12"><div className="alert alert-danger">Error: {error}</div></div>
+                    projectError &&
+                    <div className="col-12"><div className="alert alert-danger">Error: {projectError}</div></div>
                 }
                 {
-                    !error &&
-                    projects.map(project => <Project key={project.id} id={project.id} name={project.name} overdue={project.overdue} />)
+                    !projectError &&
+                    projects.map(project => <Project key={project.id} id={project.id} name={project.name} overdue={project.overdue}/>)
                 }
             </div>
         </>
--- src/config.js
+++ src/config.js
@@ -4,3 +4,4 @@ export const projectsURL = baseURL + 'projects.json';
 export const projectURL = baseURL + 'project.json';
 export const tasksURL = baseURL + 'tasks.json';
 export const taskURL = baseURL + 'task.json';
+export const clientsURL = baseURL + 'clients.json';
